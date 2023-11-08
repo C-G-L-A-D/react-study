847391be47226c26a5130d1624e790ef35be5c1a
lad <ladqmpw@163.com>
2023-11-08 11:58:43 +0800

    开启 redux-devtool

--- demo/src/store/index.js
+++ demo/src/store/index.js
@@ -1,5 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import reducer from './reducer'
 
-export default createStore(reducer, applyMiddleware(thunk))
+// 开启 redux-devtool
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        trace: true // 开启 redux-devtool 的 trace 追踪代码
+      })
+    : compose
+
+export default createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
