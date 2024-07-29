555eef33dfca2293bda86af0523eeff170a409d0
lad <ladqmpw@163.com>
2024-07-29 15:56:46 +0800

    日历组件兼容外部传入样式和类名

--- calendar-component/src/App.tsx
+++ calendar-component/src/App.tsx
@@ -3,7 +3,12 @@ import Calendar from './components/Calendar'
 function App() {
   return (
     <div className="App">
-      <Calendar value={dayjs('2022-01-01')}/>
+      <Calendar value={dayjs('2022-01-01')}
+        className={'aaa'}
+        style={{
+          background: 'yellow'
+        }}
+      />
     </div>
   );
 }
--- calendar-component/src/components/Calendar/index.tsx
+++ calendar-component/src/components/Calendar/index.tsx
@@ -2,13 +2,30 @@ import { Dayjs } from 'dayjs';
 import './index.scss';
 import MonthCalendar from './MonthCalendar';
 import Header from './Header';
+import { CSSProperties, ReactNode } from 'react';
+import cs from 'classnames';
 
 export interface CalendarProps {
-    value: Dayjs
+    value: Dayjs,
+    style?: CSSProperties,
+    className?: string | string[],
+    // 定制日期显示
+    dateRender?: (currentDate: Dayjs) => ReactNode,
+    // 定制日期单元格，内容会被添加到单元格内，只在全屏日历模式下生效
+    dateInnerContent?: (currentDate: Dayjs) => ReactNode,
+    // 国际化相关
+    locale?: string,
+    onChange?: (date: Dayjs) => void
 }
 
 function Calendar(props: CalendarProps) {
-    return <div className="calendar">
+
+    const { value, style, className } = props
+
+    // 使用 classnames 依赖库来合并类名
+    const classNames = cs("calendar", className)
+
+    return <div className={classNames} style={style}>
         <Header />
         <MonthCalendar {...props}/>
     </div>
